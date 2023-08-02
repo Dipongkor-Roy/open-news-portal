c46ec5a41bbea8daccdc979b4aaa8120316cecdb
Dipongkor-Roy <juniordipu@gmail.com>
2023-08-02 17:49:31 +0600

    server make and connected

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,9 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main/Main";
+import Home from "../Pages/Home/Home/Home";
+import Category from "../Pages/Category/Category/Category";
+import News from "../Pages/News/News/News";
 
 export const routes=createBrowserRouter([
     {
         path:'/',
-        element:<Main></Main>
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/category/:id', //:id use for uniq number for every category
+                element:<Category></Category>
+            },
+            {
+                path:'/news/:id',
+                element:<News></News>
+            }
+        ]
     }
 ])
